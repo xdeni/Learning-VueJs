677310048d344f4c5b7ec421d9dca0ecc297691e
Adenilson Araújo dos Santos <adenilsonblog@gmail.com>
2019-07-14 23:52:47 -0300

    stoped on vue cli electron

---
+++ sec5 - vue-client/simple-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Título: {{title}}</h1>
+    <h2>Autor: {{author}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Bem Vindo',
+      author: 'Adenilson Santos'
+    }
+  }
+}
+</script>
+
+<style>
+   
+</style>
